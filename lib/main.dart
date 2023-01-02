18aed913f1f3103b36d945a1721981b85e0a1b10
h.kuwabara <h.kuwabara1103@gmail.com>
2023-01-02 23:18:20 +0900

    homeウィジェットを別ファイルに切り出してinsight取得を呼び出す

--- lib/main.dart
+++ lib/main.dart
@@ -38,36 +38,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Home',
-            ),
-            ElevatedButton(
-                onPressed: () => const DetailRoute().go(context),
-                child: const Text('遷移'))
-          ],
-        ),
-      ),
-    );
-  }
-}
