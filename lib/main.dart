d167fcbad0b134fd177c37679d5f02b74f7da5fa
h.kuwabara <h.kuwabara1103@gmail.com>
2023-01-08 17:38:33 +0900

    不要なimport文削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
